91a282dae67b7b78fda46592226b006adeefde76
Taseer Baig <taseer.baig@emumba.com>
2025-08-04 17:02:02 +0500

    [ update ] - code structure update

---
+++ app/routers/users.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+# , Depends
+  # from sqlalchemy.orm import Session
+  # from app.schemas.user import UserCreate
+  # from app.services.user_service import create_user
+  # from app.dependencies import get_db
+
+router = APIRouter(prefix="/users", tags=["users"])
+
+# @router.post("/", response_model=UserCreate)
+# def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
+#     return create_user(db, user)
+
+# @router.get("/{user_id}", response_model=UserCreate)
+# def get_user(user_id: int, db: Session = Depends(get_db)):
+#     return get_user_by_id(db, user_id)
+
+#Get All users
+@router.get("/")
+def get_all_users():
+    return [{"username": "user1"}, {"username": "user2"}]
